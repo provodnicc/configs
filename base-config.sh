a7a3bb1c2f25e58689ce1b9ff59b53b0fe112a9a
provodnicc <57195517+provodnicc@users.noreply.github.com>
2022-10-30 23:55:34 +0300

    Update base-config.sh

--- base-config.sh
+++ base-config.sh
@@ -17,8 +17,6 @@ echo 'plugins=(
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
-source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 exec zsh
