59fca5e86fa40145a87f8efec3f964e3f36aa2ce
Pavel <pasha1201s@inbox.ru>
2022-10-30 22:43:44 +0300

    config zsh

---
+++ base-config.sh
@@ -0,0 +1,20 @@
+pkg install zsh vim
+
+chsh -s zsh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+echo 'plugins=(
+    git
+    zsh-autosuggestions
+    zsh-syntax-highliting
+)' >> ${ZDOTDIR:-$HOME}/.zshrc
